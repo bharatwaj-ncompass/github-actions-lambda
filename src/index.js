3dab85bb2fedcf5fca00545968b22e827424f0ae
Bharatwaj <bharatwaj@ncompass.inc>
2024-04-28 20:48:46 +0530

    editted

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-    console.log("This is a test lambda");
+    console.log("This is a test lambda edit");
     const response = {
         statusCode: 200,
         body: JSON.stringify('Lambda retun something'),
