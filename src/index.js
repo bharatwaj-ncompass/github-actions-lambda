62b17c8f097d4fb53bdbd233f56872581ecfda3e
Bharatwaj <bharatwaj@ncompass.inc>
2024-04-29 13:00:39 +0530

    update worflow

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 exports.handler = async (event) => {
-    console.log("This is a test lambda edit");
+    console.log("This is a test lambda edit 1");
     const response = {
         statusCode: 200,
         body: JSON.stringify('Lambda retun something'),
